6ff3485fe92b0b0673fb49c15d8e4b32f74c5185
semi <dbrockjr@gmail.com>
2013-06-23 20:36:13 -0500

    cleaned up print

--- dayssince.ino
+++ dayssince.ino
@@ -38,28 +38,22 @@ void loop(){
   // press
   if (p.z > __PRESURE) {
      if(numDays < 9){
-        numDays += 1;
-        itoa(numDays, buffer, 10);
-        Tft.paintScreenBlack();
-        Tft.drawString(buffer,90,40,10,WHITE);
-        Tft.drawString("Days",0,160,4,WHITE);
-        Tft.drawString("Since...",0,200,4,WHITE);
+        printScreen(90,10);
      }else if(numDays > 8 && numDays < 99){
-        numDays += 1;
-        itoa(numDays, buffer, 10);
-        Tft.paintScreenBlack();
-        Tft.drawString(buffer,40,40,10,WHITE);
-        Tft.drawString("Days",0,160,4,WHITE);
-        Tft.drawString("Since...",0,200,4,WHITE);
+        printScreen(40,10);
      }else if(numDays >= 99){
-        numDays += 1;
-        itoa(numDays, buffer, 10);
-        Tft.paintScreenBlack();
-        Tft.drawString(buffer,30,40,8,WHITE);
-        Tft.drawString("Days",0,160,4,WHITE);
-        Tft.drawString("Since...",0,200,4,WHITE);
+        printScreen(30,8);
      }
   }
 
   delay(100);
 }
+
+void printScreen(int left, int hSize){
+        numDays += 1;
+        itoa(numDays, buffer, 10);
+        Tft.paintScreenBlack();
+        Tft.drawString(buffer,left,40,hSize,WHITE);
+        Tft.drawString("Days",0,160,4,WHITE);
+        Tft.drawString("Since...",0,200,4,WHITE);
+} 
