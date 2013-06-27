53ca37de6dec9489c2466fdb81beab8a697796ff
grayisgreat <dbrockjr@gmail.com>
2013-06-26 22:38:08 -0500

    added bluetooth

--- dayssince.ino
+++ dayssince.ino
@@ -2,14 +2,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <SeeedTouchScreen.h> 
-
 #include <TFT.h>
+#include <SoftwareSerial.h>
 
 #ifdef SEEEDUINO
-  #define YP A2   // must be an analog pin, use "An" notation!
-  #define XM A1   // must be an analog pin, use "An" notation!
-  #define YM 14   // can be a digital pin, this is A0
-  #define XP 17   // can be a digital pin, this is A3 
+  #define YP A2   // A2 must be an analog pin, use "An" notation!
+  #define XM A1   // A1 must be an analog pin, use "An" notation!
+  #define YM 14   // 14 can be a digital pin, this is A0
+  #define XP 17   // 17 can be a digital pin, this is A3 
 #endif
 
 #ifdef MEGA
@@ -26,7 +26,10 @@
 
 TouchScreen ts = TouchScreen(XP, YP, XM, YM);
 int numDays = 0;
-char buffer[3];
+char buffer[3]; 
+int bluetoothTx = 18;  //A4 matches jumper
+int bluetoothRx = 19;  //A5 atches jumper
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup()
 {
@@ -34,27 +37,64 @@ void setup()
   Tft.drawString("0",90,40,10,WHITE);
   Tft.drawString("Days",0,160,4,WHITE);
   Tft.drawString("Since...",0,200,4,WHITE);
+//  Tft.drawString("0",0,240,4,RED);
+//  //Setup usb serial connection to computer
+//  Serial.begin(9600);
+//  //Setup Bluetooth serial connection to android
+//  // bluetooth.begin(38400);
+// // bluetooth.begin(115200);
+////  bluetooth.print("$$$");
+////  delay(100);
+// 
+// 
+  bluetooth.println("U,9600,N");
+  bluetooth.begin(9600);
+//  
+
 }
 
 void loop(){
- 
+  //Read from bluetooth and write to usb serial
+  if(bluetooth.available()){
+    char toSend = (char)bluetooth.read();
+    if(toSend == 'd'){ 
+      zeroScale(); 
+      //Tft.drawString("DAVE!",0,240,4,RED);
+      return;  
+    }
+    if(toSend == 'm'){
+      zeroScale();
+//     Tft.drawString("MARTY!",0,240,4,RED);
+      return;  
+    }
+    if(toSend == 'a'){
+      zeroScale();
+//      Tft.drawString("ARJAY!",0,240,4,RED);
+      return;  
+    }
+    if(toSend == 'n'){
+      zeroScale();
+//      Tft.drawString("ANDREA!",0,240,4,RED);
+      return;  
+    }
+    if(toSend == 'r'){
+      zeroScale();
+//      Tft.drawString("ROBIN!",0,240,4,RED);
+      return;  
+    }
+  }
+
   Point p = ts.getPoint();
   p.x = map(p.x, TS_MINX, TS_MAXX, 0, 240);
   p.y = map(p.y, TS_MINY, TS_MAXY, 0, 320);
   
   // press
   if (p.z > __PRESURE) {
-    numDays += 1;
-    
-    //special pressure points
-    if(p.x > 221 && p.x < 241 && p.y > 290 && p.y < 341){
-     numDays = 0;
-     printScreen(0,90,10);
-     return;   
-    }else if(p.y > 290 && p.y < 341 && p.x < 20){
-      numDays +=9; 
+    if(p.y > 290 && p.y < 341 && p.x < 20){
+      numDays +=10; 
+    }else if(p.y < 20 && p.x < 20){
+      numDays +=1; 
     }
-    
     //print days
     if(numDays < 10){
        printScreen(numDays,90,10);
@@ -64,8 +104,6 @@ void loop(){
        printScreen(numDays,30,8);
     }
   }
-  
-  delay(100);
 }
 
 void printScreen(int days, int left, int hSize){
@@ -75,3 +113,8 @@ void printScreen(int days, int left, int hSize){
   Tft.drawString("Days",0,160,4,WHITE);
   Tft.drawString("Since...",0,200,4,WHITE);
 } 
+
+void zeroScale(){
+      numDays = 0;
+      printScreen(0,90,10);
+}
